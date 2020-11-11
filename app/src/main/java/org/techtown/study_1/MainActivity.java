a6b4ce0caed3e91bb22f91b14508f1af2cc13f8f
pms940725 <pms940725@naver.com>
2020-11-12 02:35:47 +0900

    3일차 7강(Navigation Menu 기본) 강의의 프로그램 버전이랑 현재 버전이랑 생김새가 많이 달라서 시간이 오래 걸림. * NavigationDrawer의 List에 fragment_user를 추가해봄. * NavigationUI가 무엇인지 대충 알게됨.

--- app/src/main/java/org/techtown/study_1/MainActivity.java
+++ app/src/main/java/org/techtown/study_1/MainActivity.java
@@ -3,31 +3,15 @@ package org.techtown.study_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.List;
+    public class MainActivity extends AppCompatActivity {
+        private ListView list;
 
-public class MainActivity extends AppCompatActivity {
-    private ListView list;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        list = findViewById(R.id.list);   //이거 왜 추가해야 되는거야? ListView ID로 가져오는데 왜 typecasting을?
-        List<String> data = new ArrayList<>();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, data);
-        list.setAdapter(adapter);
-
-        data.add("안녕하세요.");
-        data.add("감사해요.");
-        data.add("잘있어요.");
-        data.add("다시만나요.");
-        adapter.notifyDataSetChanged();
-
-
-    }
+        }
 }
